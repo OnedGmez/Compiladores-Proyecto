c35dde84ec6e3443d1c8435b15ab71523b8bede8
OnedGmez <oned.gomez@unah.hn>
2022-10-07 11:32:34 -0600

    Actualizacion del ejemplo de compilador

--- INCISO-2_3/compilador/compilador.go
+++ INCISO-2_3/compilador/compilador.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+)
 
+var ArchivoInstrucciones []byte
+
+func main() {
+	TraerInstrucciones()
+
+}
+
+/*
+Función que servirá para llenar la variable que contiene las intrucciones GO y su equivalente en lenguaje Natural
+*/
+func TraerInstrucciones() {
+	Data, err := ioutil.ReadFile("INCISO-2_3/data_sources/Go_instrucciones.csv")
+	if err != nil {
+		fmt.Println("Error al abrir archivo")
+	} else {
+		ArchivoInstrucciones = Data
+	}
 }
